ad4db0f3fb3d2046cfb0cc73c6f02233b023e94b
Naba Kumar Das <nabad600@gmail.com>
2022-01-31 20:15:44 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('/',(req, res) => res.send(process));
-
-app.listen(3000, () => {
-    console.log('My Rest API running on port 3000!')
-})
